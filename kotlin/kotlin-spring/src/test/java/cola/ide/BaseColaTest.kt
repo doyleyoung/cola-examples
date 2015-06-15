9274de496dc49346e356f95ee57343eca87a4fed
Bruno Santos <bmrosantos@gmail.com>
2015-06-14 22:42:06 -0400

    Introduce base cola test class
    This class contains a JUnit @Test method used to trigger IDEs into running the test classes.

---
+++ kotlin/kotlin-spring/src/test/java/cola/ide/BaseColaTest.kt
@@ -0,0 +1,11 @@
+package cola.ide
+
+import org.junit.Test
+import kotlin.test.fail
+
+public open class BaseColaTest {
+    @Test
+    public fun iWillBeRemoved() {
+        fail("This test should not execute")
+    }
+}
