b8e93a4f7e825a0da3d9ac1b15dbdc1bc12f9351
Bruno Santos <bmrosantos@gmail.com>
2015-07-20 22:27:06 -0400

    Update cola-tests to 0.4.0

--- kotlin/kotlin-spring/src/test/java/cola/ide/BaseColaTest.kt
+++ kotlin/kotlin-spring/src/test/java/cola/ide/BaseColaTest.kt
@@ -1,9 +1,12 @@
 package cola.ide
 
+import com.github.bmsantos.core.cola.story.annotations.IdeEnabler
 import org.junit.Test
 import kotlin.test.fail
 
 public open class BaseColaTest {
+
+    @IdeEnabler
     @Test
     public fun iWillBeRemoved() {
         fail("This test should not execute")
