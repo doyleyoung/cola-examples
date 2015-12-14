2efcd22758e8ec55619483c34e2c923e2b296d70
Bruno Santos <bmrosantos@gmail.com>
2015-12-13 22:23:49 -0500

    Update version
    - Upgrade pom version for both Kotlin and COLA Tests

--- kotlin/kotlin-spring/src/main/java/com/github/bmsantos/kotlin/boot/Application.kt
+++ kotlin/kotlin-spring/src/main/java/com/github/bmsantos/kotlin/boot/Application.kt
@@ -2,13 +2,12 @@ package com.github.bmsantos.kotlin.boot
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import kotlin.platform.platformStatic
 
 @SpringBootApplication
 public open class Application {
     companion object {
-        platformStatic public fun main(args: Array<String>) {
-            SpringApplication.run(javaClass<Application>(), *args)
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
         }
     }
 }
